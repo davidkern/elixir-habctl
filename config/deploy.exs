5f762cc39c22f1863d0461a0433c1873d7592798
David <david@mju.io>
2020-09-28 17:11:03 -0700

    Configure bootleg to use main branch

--- config/deploy.exs
+++ config/deploy.exs
@@ -2,3 +2,5 @@ use Bootleg.DSL
 
 role :build, "10.42.0.1", silently_accept_hosts: true, user: "build", workspace: "/home/build/workspace"
 role :app, "10.42.0.1", silently_accept_hosts: true, user: "hab", workspace: "/home/hab/habctl"
+
+config :refspec, "main"
