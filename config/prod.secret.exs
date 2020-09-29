efc01e025722431a6db9b138c1ad0cc1c0b44259
David <david@mju.io>
2020-09-28 18:09:37 -0700

    Temporarily hard-code prod config while implementing deployment

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,28 +4,18 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("HABCTL_DATABASE_URL") ||
-    raise """
-    environment variable HABCTL_DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+database_url = "ecto://postgres:postgres@localhost/habctl"
 
 config :habctl, HabCtl.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: 10
 
-secret_key_base =
-  System.get_env("HABCTL_SECRET_KEY_BASE") ||
-    raise """
-    environment variable HABCTL_SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = "notverysecret"
 
 config :habctl, HabCtlWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("HABCTL_PORT") || "4000"),
+    port: 4000,
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
