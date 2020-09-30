064986daf033a2f577848daa4333a4cea03d43df
David <david@mju.io>
2020-09-29 18:03:53 -0700

    Add asset compilation step to build

--- config/deploy.exs
+++ config/deploy.exs
@@ -4,3 +4,15 @@ role :build, "10.42.0.1", silently_accept_hosts: true, user: "build", workspace:
 role :app, "10.42.0.1", silently_accept_hosts: true, user: "hab", workspace: "/home/hab/habctl"
 
 config :refspec, "main"
+
+task :phx_digest do
+  remote :build, cd: "assets" do
+    "npm install"
+    "npm run deploy"
+  end
+  remote :build do
+    "MIX_ENV=prod mix phx.digest"
+  end
+end
+
+after_task :compile, :phx_digest
